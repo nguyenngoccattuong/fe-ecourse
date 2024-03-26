21e575e2e1d0a5252083e395c67485fb62b0193a
Nguyễn Ngọc Cát Tường <109675585+nncattuong@users.noreply.github.com>
2024-03-26 15:48:24 +0700

    theme colors

--- js/main.js
+++ js/main.js
@@ -28,3 +28,58 @@ function coursePreviewVideo(){
         })
     }
 }
+coursePreviewVideo();
+
+
+// style switcher
+function styleSwitcherToggle(){
+    const styleSwitcher = document.querySelector(".js-style-switcher"),
+    styleSwitcherToggler = document.querySelector(".js-style-switcher-toggler");
+    // console.log(1);
+
+    styleSwitcherToggler.addEventListener("click", function(){
+        console.log(styleSwitcher);
+        styleSwitcher.classList.toggle("open");
+        this.querySelector("i").classList.toggle("fa-times");
+        this.querySelector("i").classList.toggle("fa-cog");
+    });
+}
+styleSwitcherToggle();
+
+
+// theme colors
+function themeColors(){
+    const colorStyle = document.querySelector(".js-color-style"),
+    themeColorsContainer = document.querySelector(".js-theme-colors");
+
+    themeColorsContainer.addEventListener("click", ({target}) => {
+        if(target.classList.contains("js-theme-color-item")){
+            localStorage.setItem("color", target.getAttribute("data-js-theme-color"));
+            setColor();
+        }
+    });
+
+    function  setColor(){
+        let path = colorStyle.getAttribute("href").split("/");
+        path = path.slice(0, path.length-1);
+        colorStyle.setAttribute("href", path.join("/") + "/" + localStorage.getItem("color") + ".css");
+
+        if(document.querySelector(".js-theme-color-item.active")){
+            document.querySelector(".js-theme-color-item.active").classList.remove("active");
+        }
+        document.querySelector("[data-js-theme-color=" + localStorage.getItem("color") + "]").classList.add("active");
+    }
+
+    if(localStorage.getItem("color") !== null){
+        setColor();
+    }
+    else{
+        const defaultColor = colorStyle.getAttribute("href").split("/").pop().split(".").shift();
+        document.querySelector("[data-js-theme-color=" + defaultColor + "]").classList.add("active");
+        
+    }
+
+}
+themeColors();
+
+
