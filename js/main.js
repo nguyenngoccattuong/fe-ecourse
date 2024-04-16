6e67d3c3d8907fab5f97f5de510f19a3dada5f17
Nguyễn Ngọc Cát Tường <109675585+nncattuong@users.noreply.github.com>
2024-04-16 19:27:23 +0700

    upload search

--- js/main.js
+++ js/main.js
@@ -263,3 +263,25 @@ $(document).ready(function() {
     console.log(answersList);
   });
 });
+
+
+
+/*=============== SEARCH ===============*/
+// const search = document.getElementById('search'),
+//       searchBtn = document.getElementById('btn-search'),
+//       searchClose = document.getElementById('btn-close')
+
+// /* Search show */
+// searchBtn.addEventListener('click', () =>{
+//    search.classList.add('show-search')
+// })
+
+// /* Search hidden */
+// searchClose.addEventListener('click', () =>{
+//    search.classList.remove('show-search')
+// })
+
+function Search(){
+    console.log('hehe');
+}
+Search();
